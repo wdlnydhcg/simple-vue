4a7a37bf15b85f8738633f44c5f8d26b17792f9a
wdlnydhcg <MrAlenZhong@163.com>
2021-12-01 17:04:56 +0800

    [MOD] 添加了双向绑定响应

--- 1 vModel/VModel.js
+++ 1 vModel/VModel.js
@@ -2,7 +2,7 @@
  * @Author: MrAlenZhong
  * @Date: 2021-12-01 08:54:16
  * @LastEditors: MrAlenZhong
- * @LastEditTime: 2021-12-01 15:22:25
+ * @LastEditTime: 2021-12-01 17:02:32
  * @Description: 
  */
 /**
@@ -14,28 +14,53 @@ class VModel {
   constructor({ el, data }) {
     this.el = document.querySelector(el);
     this._data = data; //_data 临时保存
+    this.dynNode = {}; //保存动态节点
 
     this.init();
   }
   init() {
     this.initData();
-    this.initDom(this.el)
+    this.initDom(this.el);
   }
   initDom(el) {
+    this.bindDom(el);
     this.bindInput(el);
   }
-  bindInput(el){
-    const _this = this;
-    const _allInput = el.querySelectorAll('input')
-    _allInput.forEach(item => {   //获取所有v-model的data属性
-      const _VModel = item.getAttribute('v-model');
-      if(_VModel){
-        item.addEventListener("keyup",()=>{
-          this.data[_VModel] = item.value;
-          console.log(this);
-        },false);
+  //遍历所有的dom，如果有{{}}，则添加为动态节点，方便后面替换为数据
+  bindDom(el) {
+    const childNodes = el.childNodes;
+    childNodes.forEach((item) => {
+      if (item.nodeType === 3) {
+        //节点类型为3，即文本节点
+        this.bindText(item);
       }
-    })
+      item.childNodes && this.bindDom(item); //如果有子节点就递归遍历
+    });
+  }
+  //动态节点的添加
+  bindText(el) {
+    const _this = this;
+    const _text = el.textContent;
+    const reg = /\{\{(.*)\}\}/;
+    if (reg.test(_text)) {
+      const key = _text.match(reg)[1];
+      this.dynNode[key] = el.parentNode;
+      el.parentNode.innerHTML = this.data[key]||undefined;
+    }
+  }
+  //找到所有input，如果是有v-model的，绑定事件
+  bindInput(el) {
+    const _this = this;
+    const _allInput = el.querySelectorAll("input");
+    _allInput.forEach((item) => {
+      //获取所有v-model的data属性
+      const _VModel = item.getAttribute("v-model");
+      if (_VModel) {
+        item.addEventListener("keyup", () => {
+          this.data[_VModel] = item.value;
+        });
+      }
+    });
   }
   //将数据转化为响应式数据
   initData() {
@@ -43,14 +68,16 @@ class VModel {
     this.data = {};
     for (let key in this._data) {
       // this.data;
-      console.log("this.data ", this.data);
+      // console.log("this.data ", this.data);
       Object.defineProperty(this.data, key, {
         get() {
-          console.log("获取数据",key,_this._data[key]);
+          // _this.this.bindDom(el);
+          console.log("获取数据", key, _this._data[key]);
           return _this._data[key];
         },
         set(newVal) {
-          console.log("设置数据",key,newVal);
+          console.log("设置数据", key, newVal);
+          _this.dynNode[key].innerHTML = newVal;
           _this._data[key] = newVal;
         },
       });
